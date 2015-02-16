1ff3d39450f3a17485c3d09c2592e77ed344bf22
Caleb Wright <awright@rentpath.com>
2015-02-16 14:41:37 -0500

    [CW] [88373832] Change bower ref to jquery 1.8.3

--- spec/javascripts/config/bootstrap.js
+++ spec/javascripts/config/bootstrap.js
@@ -2,7 +2,7 @@ require.config({
   waitSeconds: "120",
   baseUrl: "/vendor/bower/",
   paths: {
-    "jquery": "jquery/dist/jquery",
+    "jquery": "jquery/jquery",
     "jquery.cookie": "jquery.cookie/jquery.cookie",
     "jasmine-jquery": "jasmine-jquery/lib/jasmine-jquery",
     "primedia_events": "primedia_events/primedia-events"
